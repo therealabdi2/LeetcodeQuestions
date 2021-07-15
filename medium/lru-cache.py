b9384c6016b9606903b955c8415a1cb15434c8f4
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-07-16 02:16:52 +0500

    adds lru_cache.py question

---
+++ medium/lru-cache.py
@@ -0,0 +1,85 @@
+"""Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+
+
+Example 1:
+
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4"""
+
+from collections import deque
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.c = capacity
+        self.m = dict()
+        self.deq = deque()
+
+    def get(self, key: int) -> int:
+        if key in self.m:
+            value = self.m[key]
+            self.deq.remove(key)
+            self.deq.append(key)
+            return value
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.m:
+            if len(self.deq) == self.c:
+                oldest = self.deq.popleft()
+                del self.m[oldest]
+        else:
+            self.deq.remove(key)
+
+        self.m[key] = value
+        self.deq.append(key)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+s = LRUCache(3)  # 3 is capacity of our cache
+
+s.put(1, 1000)
+s.put(2, 2000)
+s.put(3, 3000)
+
+# [1, 2, 3] should be the structure at this moment
+
+print(s.get(1))
+# now order will be [2, 3, 1]  1 moved to the front of our cache
+
+s.put(4, 4000)
+# now cache will be [3, 1, 4] 2 was the oldest value so it got removed and 4 is the latest
+# least recent <=[3, 1, 4]=> most recent
+print(s.get(2))  # -1 dosent exist in cache anymore
+
+print(s.get(3))  # 3000
+# [1, 4, 3] now 3 is our latest
