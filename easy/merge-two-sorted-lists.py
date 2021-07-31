c3e2a884fa3996d912b79543c66514b10fcb9b13
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-08-01 01:23:52 +0500

    add merge-two-sorted-lists.py

---
+++ easy/merge-two-sorted-lists.py
@@ -0,0 +1,76 @@
+'''Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+
+
+Example 1:
+
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = ListNode(0)
+        ans = cur
+        while l1 and l2:
+            if l1.val > l2.val:
+                cur.next = l2
+                l2 = l2.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+
+            cur = cur.next
+
+        while l1:
+            cur.next = l1
+            l1 = l1.next
+            cur = cur.next
+        while l2:
+            cur.next = l2
+            l2 = l2.next
+            cur = cur.next
+
+        return ans.next
+
+
+s = Solution()
+
+# li: 1->2->4
+l1_1 = ListNode(1)
+l1_2 = ListNode(2)
+l1_4 = ListNode(4)
+
+l1_1.next = l1_2
+l1_2.next = l1_4
+
+# li: 1->3->4
+
+l2_1 = ListNode(1)
+l2_3 = ListNode(3)
+l2_4 = ListNode(4)
+
+l2_1.next = l2_3
+l2_3.next = l2_4
+
+ans = s.mergeTwoLists(l1_1, l2_1)
+
+while ans:
+    print(ans.val)
+    ans = ans.next
