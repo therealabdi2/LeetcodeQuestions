7e4d92a91407a10bce1b3ae85829722dbb71cc0f
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-09-04 15:30:04 +0500

    min_stack implementation

---
+++ easy/min_stack.py
@@ -0,0 +1,59 @@
+'''
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+'''
+
+
+class MinStack:
+    def __init__(self):
+        self.st = []
+
+    def push(self, x: int) -> None:
+        curMin = self.getMin()
+        if curMin == None or curMin > x:
+            curMin = x
+        self.st.append((x, curMin))
+
+    def pop(self) -> None:
+        self.st.pop()
+
+    def top(self) -> int:
+        return self.st[-1][0] if self.st else None
+
+    def getMin(self) -> int:
+        return self.st[-1][1] if self.st else None
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
