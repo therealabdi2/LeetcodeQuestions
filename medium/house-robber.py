19c2c09bd3b0b806906acf27a8f6ace196326e56
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-09-09 01:05:14 +0500

    house-robber implementation

---
+++ medium/house-robber.py
@@ -0,0 +1,43 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+'''
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n is 0:
+            return 0
+
+        dp = [0] * n  # this will store the maximum money we can steal upto a specific house
+        dp[0] = nums[0]  # the first cell will be whatever money is in the first house
+
+        for i in range(1, n):
+            # if this is the second house we can calc the max we can steal by comparing with house#1
+            if i == 1:
+                dp[i] = max(nums[0], nums[1])
+            else:
+                # for other generic cases
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        # the last pos in the array contains max amount we can steal
+        return dp[-1]
