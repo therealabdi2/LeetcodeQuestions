83b88276827186836cb0c78526d2b886660e25b5
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-09-14 23:39:49 +0500

    trapping-rain-water implementation

--- hard/trapping-rain-water.py
+++ hard/trapping-rain-water.py
@@ -7,12 +7,14 @@ Example 1:
 
 Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
-Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+In this case, 6 units of rain water are being trapped.
 Example 2:
 
 Input: height = [4,2,0,3,2,5]
 Output: 9
 '''
+
 from typing import List
 
 
@@ -25,7 +27,7 @@ class Solution:
         res = 0
 
         while l < r:
-            if left_max < right_max:
+            if left_max <= right_max:
                 l += 1
                 left_max = max(left_max, height[l])
                 # since we are checking max already we dont need to check for -ve values here
