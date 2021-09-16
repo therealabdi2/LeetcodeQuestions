7cbe43f53000d8dc38adc16fe83a3e8b78a2226c
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-09-17 01:11:22 +0500

    add 2nd solution for Longest Substring Without Repeating Characters implementation

--- medium/Longest Substring Without Repeating Characters.py
+++ medium/Longest Substring Without Repeating Characters.py
@@ -48,5 +48,25 @@ class Solution:
         return ans
 
 
+class Solution2:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        l = 0
+        res = 0  # will store the max len
+
+        # the right pointer will go through every single char
+        for r in range(len(s)):
+            # if we get to a duplicate char then we have to update our window and our set
+            while s[r] in char_set:  # meaning the char is a duplicate
+                char_set.remove(s[l])  # removing the left most char
+                l += 1  # inc pointer
+            char_set.add(s[r])  # after we remove duplicates we can add it to our set
+            # at this point we know for sure there are no duplicates in the set so update result
+            res = max(res, r - l + 1)
+        return res
+
+
 s = Solution()
-print(s.lengthOfLongestSubstring("fivestartreview"))
+s2 = Solution2()
+print(s.lengthOfLongestSubstring("abcabcbb"))
+print(s2.lengthOfLongestSubstring("abcabcbb"))
