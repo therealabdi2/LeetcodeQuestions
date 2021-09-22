279dfbb9121076c380918e9667afe3d4633361c6
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-09-23 00:13:47 +0500

    add meeting-rooms-ii implementation

---
+++ medium/meeting-rooms-II.py
@@ -0,0 +1,53 @@
+'''
+Description
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+find the minimum number of conference rooms required.
+
+Example
+Example1
+
+Input: intervals = [(0,30),(5,10),(15,20)]
+Output: 2
+Explanation:
+We need two meeting rooms
+room1: (0,30)
+room2: (5,10),(15,20)
+Example2
+
+Input: intervals = [(2,7)]
+Output: 1
+Explanation:
+Only need one meeting room
+'''
+
+
+# Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+
+    def minMeetingRooms(self, intervals):
+        # Write your code here
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res, count = 0, 0
+        s, e = 0, 0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
