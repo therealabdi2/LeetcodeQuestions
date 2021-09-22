90df37c868ae7cdc7558da2a9884313a8535eb5a
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-09-23 01:05:52 +0500

    add insert-interval.py implementation

---
+++ medium/insert-interval.py
@@ -0,0 +1,62 @@
+'''
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi]
+represent the start and the end of the ith interval and intervals is sorted in ascending order by starti.
+You are also given an interval newInterval = [start, end]
+that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti
+and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+Example 3:
+
+Input: intervals = [], newInterval = [5,7]
+Output: [[5,7]]
+Example 4:
+
+Input: intervals = [[1,5]], newInterval = [2,3]
+Output: [[1,5]]
+Example 5:
+
+Input: intervals = [[1,5]], newInterval = [2,7]
+Output: [[1,7]]
+'''
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+                # we wont append the new interval yet because it could still overlap with other intervals
+            else:
+                # this condition is for overlapping with current interval
+                # in this case we merge it
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                # we still dont append because this new interval could still overlap with other intervals on the right
+
+        # here we append just in case the first if condition doesnt execute
+        res.append(newInterval)
+        return res
+
+
+s = Solution()
+print(s.insert(intervals=[[1, 3], [6, 9]], newInterval=[2, 6]))
