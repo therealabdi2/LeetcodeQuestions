1a640b146f4bc08de44dec252363fe977436aa2c
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-10-02 01:39:53 +0500

    add recursive, bfs and dfs solutions to maximum_depth_of_binary_tree implementation

--- easy/maximum_depth_of_binary_tree.py
+++ easy/maximum_depth_of_binary_tree.py
@@ -1,6 +1,8 @@
-'''Given the root of a binary tree, return its maximum depth.
+'''
+Given the root of a binary tree, return its maximum depth.
 
-A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+A binary tree's maximum depth is the number of nodes along the longest path
+from the root node down to the farthest leaf node.
 
 
 
@@ -20,9 +22,11 @@ Output: 0
 Example 4:
 
 Input: root = [0]
-Output: 1'''
+Output: 1
+'''
 
 # Definition for a binary tree node.
+from collections import deque
 from typing import Optional
 
 
@@ -44,8 +48,57 @@ class Solution:
         return max(left, right) + 1
 
 
+class Solution2:
+    # recursion O(n)
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+class Solution3:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # iterative BFS, not really good its the same time complexity as recursion
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+        while q:
+
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
+        return level
+
+
+class Solution4:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # iterative preorder DFS, without recursion same time and space compexity
+        stack = [[root, 1]]
+        res = 0
+        while stack:
+            # we are getting 2 values here
+            node, depth = stack.pop()
+
+            # if node is not null we do
+            if node:
+                res = max(res, depth)
+                # the children can be null but the if statement will prevent us from using it
+                # so null nodes are ignored
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+
 def main1(root):
-    s = Solution()
+    s = Solution4()
     maxDepth = s.maxDepth(root)
     print(maxDepth)
 
