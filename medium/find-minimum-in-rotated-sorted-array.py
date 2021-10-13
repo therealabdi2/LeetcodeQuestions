850f871d977bc8729638f0382bbb4f8abebab848
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-10-14 01:44:40 +0500

    add find-minimum-in-rotated-sorted-array implementation

---
+++ medium/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,58 @@
+'''
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ...,
+a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+'''
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            # if array is sorted or comes to a sorted position
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            # now we need to check if this mid value is a part of the left sorted portion
+            # it is if it is greater than the left pointer value
+            # else its in the right and we just take the left portion values into consideration
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
+
+s = Solution()
+print(s.findMin(nums=[3, 4, 5, 1, 2]))
