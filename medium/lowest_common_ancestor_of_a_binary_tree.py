43b4afaaf068bf732cefa6c619dd80ede88e26b6
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2021-11-05 00:47:26 +0500

    add 2nd solution lowest_common_ancestor_of_a_binary_tree implementation

--- medium/lowest_common_ancestor_of_a_binary_tree.py
+++ medium/lowest_common_ancestor_of_a_binary_tree.py
@@ -1,6 +1,10 @@
-'''Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+'''
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia:
+“The lowest common ancestor is defined between two nodes p and q as
+the lowest node in T that has both p and q
+as descendants (where we allow a node to be a descendant of itself).”
 
 
 
@@ -15,7 +19,8 @@ Example 2:
 
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
 Output: 5
-Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Explanation: The LCA of nodes 5 and 4 is 5, since
+a node can be a descendant of itself according to the LCA definition.
 Example 3:
 
 Input: root = [1,2], p = 1, q = 2
@@ -52,3 +57,30 @@ class Solution:
         if left is None:
             return right
         return left
+
+
+class Solution2:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root.val == p.val or root.val == q.val:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
+
+
+node = TreeNode(6)
+node.left = TreeNode(2)
+node.right = TreeNode(8)
+node.left.left = TreeNode(0)
+node.left.right = TreeNode(4)
+node.right.left = TreeNode(7)
+node.right.right = TreeNode(9)
+node.left.left.left = TreeNode(3)
+node.left.left.right = TreeNode(5)
+
+s = Solution2()
+print(s.lowestCommonAncestor(node, node.right, node.left).val)
