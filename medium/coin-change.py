835bbc9d7d28e29eeb039befff9b0805dc4f0557
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-01-01 01:49:45 +0500

    add 2nd solution for coin-change implementation

--- medium/coin-change.py
+++ medium/coin-change.py
@@ -55,5 +55,19 @@ class Solution:
 
         return dp[amount] if dp[amount] != float('inf') else -1
 
+
+class Solution2:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    dp[a] = min(dp[a], dp[a - coin] + 1)
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+
 s = Solution()
 print(s.coinChange([1, 2, 5], 11))
