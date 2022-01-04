62c2b3dff4f79ba48ddde8177aecbe3bb28b895f
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-01-05 01:28:25 +0500

    add sum-of-two-integers implementation

---
+++ medium/sum-of-two-integers.py
@@ -0,0 +1,34 @@
+'''
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+'''
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
+        return a if a <= MAX else ~(a ^ mask)
+
+
+s = Solution()
+print(s.getSum(1, 2))
