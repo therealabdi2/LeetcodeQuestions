860c456240607fcc4b70494ac9da27feda938ca3
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-02-05 00:27:25 +0500

    update remove-nth-node-from-end-of-list.py implementation

--- medium/remove-nth-node-from-end-of-list.py
+++ medium/remove-nth-node-from-end-of-list.py
@@ -28,19 +28,29 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # time complexity: O(n)
+        # space complexity: O(1)
+        # we use a dummy node to make the head node easier to handle
         dummy = ListNode(0, head)
         left = dummy
         right = head
 
+        # move right pointer n steps ahead
         while n > 0 and right:
             right = right.next
             n -= 1
 
+        # we keep moving till right is None
         while right:
             left = left.next
             right = right.next
 
-        # delete
+        # delete the node
         left.next = left.next.next
 
         return dummy.next
+
+
+s = Solution()
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+print(s.removeNthFromEnd(head, 2))
