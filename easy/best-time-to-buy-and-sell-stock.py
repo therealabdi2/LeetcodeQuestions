658e265e956aeb5ce6dce5802fa9863a4c98d516
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-07-01 22:59:18 +0500

    add solution3 to best-time-to-buy-and-sell-stock.py implementation

--- easy/best-time-to-buy-and-sell-stock.py
+++ easy/best-time-to-buy-and-sell-stock.py
@@ -55,5 +55,21 @@ class Solution2:
         return profit
 
 
+class Solution3:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        profit = 0
+
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+            else:
+                profit = max(profit, prices[r] - prices[l])
+
+            r += 1
+
+        return profit
+
+
 s = Solution2()
 print(s.maxProfit(prices=[7, 1, 5, 3, 6, 4]))
